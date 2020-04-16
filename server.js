7a5e33ee8d70e2e1dedcc82f2be8c9453a360de8
Bailey Pownell <bailey.pownell@gmail.com>
2020-04-16 17:05:38 -0400

    configuring node for hosting

---
+++ server.js
@@ -0,0 +1,14 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.static(__dirname + '/dist'));
+
+app.get('*', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.listen(port, ()=>{
+  console.log(`server is up on port ${port}`);
+});
