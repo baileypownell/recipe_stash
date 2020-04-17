d9628e7b6b63ed8b5029dcb29625a0349674b90b
Bailey Pownell <bailey.pownell@gmail.com>
2020-04-17 17:13:37 -0400

    configuring...

--- webpack.config.js
+++ webpack.config.js
@@ -4,26 +4,30 @@ const fs = require('fs'); // to check if the file exists
 const path = require('path'); // to get the current path
 
 module.exports = (env) => {
-  // Get the root path (assuming your webpack config is in the root of your project!)
-  const currentPath = path.join(__dirname);
-
-  // Create the fallback path (the production .env)
-  const basePath = currentPath + '/.env';
-
-  // We're concatenating the environment name to our filename to specify the correct env file!
-  const envPath = basePath + '.' + env.ENVIRONMENT;
-
-  // Check if the file exists, otherwise fall back to the production .env
-  const finalPath = fs.existsSync(envPath) ? envPath : basePath;
-
-  // Set the path parameter in the dotenv config
-  const fileEnv = dotenv.config({ path: finalPath }).parsed;
-
-  // reduce it to a nice object, the same as before (but with the variables from the file)
-  const envKeys = Object.keys(fileEnv).reduce((prev, next) => {
-    prev[`process.env.${next}`] = JSON.stringify(fileEnv[next]);
-    return prev;
-  }, {});
+  // // Get the root path (assuming your webpack config is in the root of your project!)
+  // const currentPath = path.join(__dirname);
+  //
+  // // Create the fallback path (the production .env)
+  // const basePath = currentPath + '/.env';
+  //
+  // // We're concatenating the environment name to our filename to specify the correct env file!
+  // const envPath = basePath + '.' + env.ENVIRONMENT;
+  // console.log('the envPath:', envPath)
+  //
+  // // Check if the file exists, otherwise fall back to the production .env
+  // const finalPath = fs.existsSync(envPath) ? envPath : basePath;
+  // console.log(finalPath)
+  //
+  // // Set the path parameter in the dotenv config
+  // const fileEnv = dotenv.config({ path: finalPath }).parsed;
+  // console.log(fileEnv)
+  //
+  // // reduce it to a nice object, the same as before (but with the variables from the file)
+  // const envKeys = Object.keys(fileEnv).reduce((prev, next) => {
+  //   prev[`process.env.${next}`] = JSON.stringify(fileEnv[next]);
+  //   return prev;
+  // }, {});
+  // console.log('envKeys', envKeys)
 return {
   entry: './src/index.js',
   output: {
@@ -60,9 +64,8 @@ return {
     ]
   },
   plugins: [
-    //new webpack.DefinePlugin(envKeys)
     new webpack.DefinePlugin({
-      'process.env.NODE_ENV': process.env.NODE_ENV
+      PRODUCTION: JSON.stringify(true),
     })
   ],
   devServer: {
