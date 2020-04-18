3cb536214a4292c34c27855908754d426fc5f01e
Bailey Pownell <bailey.pownell@gmail.com>
2020-04-18 15:20:02 -0400

    a

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
-const routes = require('/routes');
+const routes = require('./routes');
 
 
 // middleware
