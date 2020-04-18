81d9d34e57a190e7e46099a75c423e202429f248
Bailey Pownell <bailey.pownell@gmail.com>
2020-04-18 15:10:48 -0400

    changed import

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
-const routes = require('api/routes');
+const routes = require('/api/routes');
 
 
 // middleware
