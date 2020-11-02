b52130d954e85c8ca4f0ab15e573c298963ca099
Bailey Pownell <bailey.pownell@gmail.com>
2020-11-02 13:05:26 -0500

    new modal updates

--- src/components/Dashboard/Category/AddRecipe/AddRecipe.js
+++ src/components/Dashboard/Category/AddRecipe/AddRecipe.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import M from 'materialize-css';
+import './AddRecipe.scss';
 
 class AddRecipe extends React.Component {
 
   state = {
-    showModal: false
+    recipe_title: null,
+    ingredients: null,
+    directions: null,
+    loading: null,
+    category: this.props.category,
+    recipeValid: false
   }
 
   componentDidMount() {
     var elems = document.querySelectorAll('.modal');
-    var instances = M.Modal.init(elems, {
+    M.Modal.init(elems, {
       opacity: 0.5
     });
 
@@ -17,17 +23,57 @@ class AddRecipe extends React.Component {
     M.FormSelect.init(select, {});
   }
 
-  closeModal = () => {
+  checkValidity = () => {
+    const { directions, ingredients, recipe_title } = this.state;
+    if (directions && ingredients && recipe_title) {
+      console.log('valid')
+      this.setState({
+        recipeValid: true
+      })
+    } else {
+      console.log('invalid')
+      this.setState({
+        recipeValid: false
+      })
+    }
+  }
+
+  createRecipe = (e) => {
+    e.preventDefault();
     this.setState({
-      showModal: false
+      loading: true
+    })
+    axios.post(`/recipes`, {
+      title: this.state.recipe_title,
+      category: this.state.category,
+      ingredients: this.state.ingredients,
+      directions: this.state.directions,
+    })
+    .then(res => {
+      if (res) {
+        this.setState({
+          loading: false
+        });
+        this.props.closeModal();
+        this.props.updateDashboard();
+        M.toast({html: 'Recipe added.'})
+      }
+    })
+    .catch((err) => {
+      this.setState({
+        loading: false
+      })
+      M.toast({html: 'There was an error.'})
     })
   }
 
-  addRecipe = () => {
+  updateInput = (e) => {
     this.setState({
-      showModal: true
-    })
+      [e.target.id]: e.target.value
+    }, () => this.checkValidity());
   }
+
+  
   render() {
     const { id, category } = this.props;
     const options = [
@@ -46,7 +92,7 @@ class AddRecipe extends React.Component {
             data-target="modal1"
             className="addRecipe z-depth-4 modal-trigger"
             id={id}
-            onClick={this.addRecipe} >
+             >
             <i className="fas fa-plus-circle"></i>
         </div>
 
@@ -56,23 +102,23 @@ class AddRecipe extends React.Component {
               <div className="recipe">
               <div>
                   <div className="input-field">
-                      <textarea id="title" className="materialize-textarea"></textarea>
-                      <label htmlFor="title">Title</label>
+                      <textarea onChange={this.updateInput} id="recipe_title" value={this.state.recipe_title || ''} className="materialize-textarea"></textarea>
+                      <label htmlFor="recipe_title">Title</label>
                   </div>
                   <div className="input-field">
-                      <textarea id="ingredients" className="materialize-textarea"></textarea>
+                      <textarea onChange={this.updateInput} id="ingredients" value={this.state.ingredients || ''} className="materialize-textarea minHeight"></textarea>
                       <label htmlFor="ingredients">Ingredients</label>
                   </div>
 
                   <div className="input-field">
-                    <textarea id="directions" className="materialize-textarea"></textarea>
+                    <textarea onChange={this.updateInput} id="directions" value={this.state.directions || ''} className="materialize-textarea minHeight"></textarea>
                     <label htmlFor="directions">Directions</label>
                   </div>
               
                   <div >
                     <h3>Category</h3>
                     <div className="select">
-                      <select>
+                      <select onChange={this.updateInput} id="category" value={this.state.category}>
                         {
                           options.map((val, index) => {
                             return <option key={index}>{val.label}</option>
@@ -82,13 +128,21 @@ class AddRecipe extends React.Component {
                         
                     </div>
                 </div>
-                <label>
+                {/* <label>
                   <input type="checkbox" class="filled-in" />
                   <span>No baking required</span>
-                </label>
+                </label> */}
                 
               </div>
-              <button className="waves-effect waves-light btn" onClick={this.updateCategory}>Save</button>
+              <div className="modal-close-buttons">
+                <button className="modal-close btn waves-effect waves-light grayBtn">Cancel</button>
+                <button 
+                  className={!this.state.recipeValid ? 'waves-effect waves-light btn disabled' : 'waves-effect waves-light btn enabled'}
+                  disabled={!this.state.recipeValid} 
+                  onClick={this.createRecipe}>Save
+                 </button>
+              </div>
+              
             </div>
           </div> 
       </>
