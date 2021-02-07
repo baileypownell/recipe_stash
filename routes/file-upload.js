c0512a7f56b491eabf171f697aa0d3cd7184b52b
Bailey Pownell <bailey.pownell@gmail.com>
2021-02-07 12:39:54 -0500

    solution that doesn't involve saving to disk

--- routes/file-upload.js
+++ routes/file-upload.js
@@ -3,8 +3,9 @@ const { Router } = require('express')
 const client = require('../db')
 const router = Router()
 const aws = require('aws-sdk')
-const uuid = require('uuid')
-const multer = require('multer');
+const { v4: uuidv4 } = require('uuid');
+const multer = require('multer')
+const multerS3 = require('multer-s3')
 const fs = require('fs')
 const environment = process.env.NODE_ENV || 'development';
 
@@ -14,46 +15,49 @@ if (environment === 'development') {
     })
   }
 
-const s3 = new aws.S3({
+
+aws.config.update({
     secretAccessKey: process.env.S3_ACCESS_SECRET, 
     accessKeyId: process.env.S3_ACCESS_KEY_ID, 
-    signatureVersion: 'v4',
     region: 'us-east-2'
 })
 
-const storage = multer.diskStorage({
-    destination: './uploads', 
-    fileName: function(req, file, callback) {
-      callback(null, `${file.originalname}`)
-    }
-  })
-  
-  const upload = multer({
-    storage: storage, 
-  }).single('image')
+const s3 = new aws.S3()
 
+const upload = multer({
+    storage: multerS3({
+        s3, 
+        bucket: 'virtualcookbook-media',
+        contentType: multerS3.AUTO_CONTENT_TYPE,
+        metadata: function (req, file, cb) {
+            cb(null, {fieldName: file.fieldname})
+        }, 
+        key: function (req, file, cb) {
+            cb(null, req.s3Key)
+        }
+    })
+})
+const singleFileUpload = upload.single('image')
+
+function uploadToS3(req, res) {
+    req.s3Key = uuidv4()
+    let downloadUrl = `https://s3-us-east-2.amazonaws.com/virtualcookbook-media/${req.s3Key}`
+    return new Promise((resolve, reject) => {
+        return singleFileUpload(req, res, err => {
+            if (err) return reject(err)
+            return resolve(downloadUrl)
+        }) 
+    })
+}
 
 router.post('/', (req, res) => {
-    upload(req, res, (err) => {
-        if (err) {
-            res.json({error: true})
-            return
-        }
-        console.log(req.file)
-        let params = {
-            Bucket: 'virtualcookbook-media',
-            Key: uuid.v4(),
-            Body: fs.createReadStream(`uploads/${req.file.filename}`), 
-            ContentType: req.file.mimetype, 
-        }
-    
-        s3.upload(params, (err, data) => {
-            if (err) {
-                console.log(err)
-            } else {
-                return res.status(200).json({ success: true })
-            }
-        })
+    uploadToS3(req, res)
+    .then(downloadUrl => {
+        // update the db
+        return res.status(200).send({ downloadUrl })
+    })
+    .catch(e => {
+        console.log(e)
     })
 })
 
