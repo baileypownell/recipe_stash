4cc09716d42b2689532f76c6f9eff31bc7433c3d
Bailey Pownell <bailey.pownell@gmail.com>
2021-02-13 12:30:08 -0500

    UI updates

--- src/components/File-Upload/FileUpload.js
+++ src/components/File-Upload/FileUpload.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import './FileUpload.scss';
+import './FileUpload.scss'
+const { v4: uuidv4 } = require('uuid')
 
 class FileUpload extends React.Component {
 
@@ -8,37 +9,66 @@ class FileUpload extends React.Component {
     }
     
     openFileFinder = () => {
-        console.log(input)
-        input.click()
+        this.input.click()
     }
 
     handleDrop = (e) => {
         e.preventDefault()
         e.stopPropagation()
-        console.log(e.dataTransfer)
         if (e.dataTransfer.files && e.dataTransfer.items.length >=1 ) {
-            console.log(e.dataTransfer.files)
+            let currentFiles = this.state.files
+            Array.from(e.dataTransfer.files).forEach(file => {
+                currentFiles.push({
+                    file: file,
+                    id: uuidv4()
+                })
+            })
             this.setState({
-                files: e.dataTransfer.files
-            }, () => console.log(this.state.files))
+                files: currentFiles
+            })
         }
     }
 
-    removeFile = (file) => {
-        // e.stopPropagation()
-        // e.preventDefault()
-        console.log(file)
+    handleUpload = (e) => {
+     
+            let currentFiles = this.state.files
+            Array.from(e.target.files).forEach(file => {
+                currentFiles.push({
+                    file: file,
+                    id: uuidv4()
+                })
+            })
+            this.setState({
+                files: currentFiles
+            })
+        
+    }
+
+    removeFile = (fileId) => {
+        let updatedFileList = [] 
+        this.state.files.forEach(file => {
+            if (file.id !== fileId) {
+                updatedFileList.push(file)
+            }
+        })
+        this.setState({
+            files: updatedFileList
+        })
     }
 
     input = React.createRef()
 
     render() {
-        
-
         return (
             <div className="file-upload">
                 <div className="dropzone" onDrop={this.handleDrop} onDragOver={this.handleDrop}>
-                <input ref={i => this.input = i} type="file" id="input" multiple></input>
+                <input 
+                    ref={i => this.input = i} 
+                    type="file" 
+                    id="input" 
+                    onChange={this.handleUpload}
+                    multiple>
+                 </input>
                     <div>
                         <h1>Drag & Drop an image</h1>
                         <button onClick={this.openFileFinder} className="waves-effect waves-light btn">Choose a file</button>
@@ -51,10 +81,15 @@ class FileUpload extends React.Component {
                 { this.state.files.length > 0 ?
                 <div class="file-list">
                     {Array.from(this.state.files).map(file => {
-                        return <div class="file-name">
-                            <span>{file?.name}</span> 
-                            <i onClick={(e) => this.removeFile(file)} className="fas fa-trash"></i>
-                        </div>
+                        return (
+                            <div className="file-preview">
+                                <div className="file-name">
+                                    <span>{file.file?.name}</span> 
+                                    <i onClick={(e) => this.removeFile(file.id)} className="fas fa-trash"></i>
+                                </div>
+                                <img src={URL.createObjectURL(file.file)} />
+                            </div>
+                        )
                     })}
                  
                 </div>
