a33c6b762f78188666adcc02017e63d4e7ac2549
Bailey Pownell <bailey.pownell@gmail.com>
2021-03-02 12:41:50 -0500

    fix

--- src/components/Preloader/Preloader.js
+++ src/components/Preloader/Preloader.js
@@ -1,19 +1,18 @@
+import React from 'react';
 
-
-function Preloader(props) {
-    return (
-        <div className="preloader-wrapper small active">
-            <div className="spinner-layer">
-            <div className="circle-clipper left">
-                <div className="circle"></div>
-            </div><div className="gap-patch">
-                <div className="circle"></div>
-            </div><div className="circle-clipper right">
-                <div className="circle"></div>
-            </div>
-            </div>
+const Preloader = (props) => (
+    <div className="preloader-wrapper small active">
+        <div className="spinner-layer">
+        <div className="circle-clipper left">
+            <div className="circle"></div>
+        </div><div className="gap-patch">
+            <div className="circle"></div>
+        </div><div className="circle-clipper right">
+            <div className="circle"></div>
         </div>
-    )
-}
+        </div>
+    </div>
+)
+
 
 export default Preloader;
