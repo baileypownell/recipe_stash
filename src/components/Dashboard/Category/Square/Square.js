580ae618ea0f8cd35dd7c924062962779208ab4b
Bailey Pownell <bailey.pownell@gmail.com>
2021-05-24 12:52:33 -0400

    added skeleton rendering

--- src/components/Dashboard/Category/Square/Square.js
+++ src/components/Dashboard/Category/Square/Square.js
@@ -2,11 +2,14 @@ import React from 'react';
 import { withRouter } from "react-router-dom";
 import './Square.scss'
 import Fade from 'react-reveal/Fade'
+import Skeleton from 'react-loading-skeleton';
 
 class Square extends React.Component {
 
   state = {
-    imageLoaded: false
+    imageLoaded: false,
+    skeletonWidth: 120, 
+    skeletonHeight: 120
   }
 
   viewRecipe = () => {
@@ -19,11 +22,36 @@ class Square extends React.Component {
     })
   }
 
+  componentDidMount() {
+    window.addEventListener('resize', this.handleWindowSizeChange)
+    this.handleWindowSizeChange()
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.handleWindowSizeChange)
+  }
+
+  handleWindowSizeChange = () => {
+    let skeletonHeight, skeletonWidth
+    if (window.innerWidth >= 600) {
+      skeletonHeight = 100 
+      skeletonWidth = 175
+    } else {
+      skeletonHeight = 120 
+      skeletonWidth = 120
+    } 
+    this.setState({
+      skeletonWidth,
+      skeletonHeight
+    })
+  }
+
   // a <Square/> should not render until the background image (if there is one) is fully loaded 
   // this means, we need to technically render an <img/> so that we can react with the onLoad listener 
   // then, render the div 
   render() {
     const { data, key, rawTitle, awsUrl } = this.props;
+    const { skeletonHeight, skeletonWidth } = this.state;
     return (
       <>
       { !!awsUrl ? 
@@ -42,11 +70,14 @@ class Square extends React.Component {
           </div>
           </Fade>
            : 
-          <img
-            src={awsUrl}
-            style={{ display: `none`}}
-            onLoad={this.triggerDiv}
-            />
+           <>
+              <img
+                src={awsUrl}
+                style={{ display: `none`}}
+                onLoad={this.triggerDiv}
+                />
+               <Skeleton width={skeletonWidth} height={skeletonHeight} className="skeleton" /> 
+          </>
           }
         </>
       : 
