dce0c952bdf8419020a8025cd9ae7283a26a9ae2
Bailey Pownell <bailey.pownell@gmail.com>
2021-09-05 09:47:24 -0400

    new approach

---
+++ src/components/App/App.tsx
@@ -0,0 +1,101 @@
+import React from 'react'
+import AuthenticationService from '../../services/auth-service'
+import { QueryClientProvider, QueryClient } from 'react-query'
+import { ThemeProvider, createTheme } from '@material-ui/core'
+import { BrowserRouter, Switch, Route, Redirect, withRouter } from 'react-router-dom'
+import { Nav, Home, Login, ResetPassword, Signup, Settings } from '..'
+import GuardedRoute from '../../GuardedRoute'
+import RecipeCache from '../RecipeCache/RecipeCache'
+
+export const queryClient = new QueryClient()
+
+class App extends React.Component<any> {
+    state = {
+      authenticationStateDetermined: false,
+      loggedIn: null
+    }
+
+    componentDidMount () {
+      AuthenticationService.verifyUserSession()
+        .then(res => {
+          if (res.data.authenticated) {
+            this.setState({
+              authenticationStateDetermined: true,
+              loggedIn: true
+            }, () => {
+              window.localStorage.setItem('user_logged_in', 'true')
+            })
+          } else {
+            this.setState({
+              authenticationStateDetermined: true,
+              loggedIn: false
+            }, () => {
+              window.localStorage.removeItem('user_logged_in')
+            })
+            // this.props.history.push('/')
+          }
+        })
+        .catch(err => console.log(err))
+    }
+
+    render () {
+      const theme = createTheme({
+        palette: {
+          primary: {
+            main: '#e66c6c',
+            dark: '#d35151'
+          },
+          secondary: {
+            main: '#87ad6a',
+            contrastText: '#fff'
+          },
+          error: {
+            main: '#dd7244',
+            dark: '#c23c3c'
+          },
+          info: {
+            main: '#dbdbdb'
+          }
+        }
+      })
+
+      // if (!this.state.authenticationStateDetermined) return null
+
+      // if (this.state.loggedIn !== null && this.state.loggedIn === false) {
+      //   console.log('not logged in')
+      //   return null
+      // }
+
+      return (
+        <QueryClientProvider client={queryClient}>
+            <ThemeProvider theme={theme}>
+            <BrowserRouter>
+                <Nav />
+                <Switch>
+                <Route exact={true} path="/" component={Home}/>
+                <Route path="/login" component={Login}/>
+                <Route path="/signup" component={Signup}/>
+                <Route path="/reset/:token" component={ResetPassword}/>
+                <GuardedRoute
+                    path='/recipes'
+                    exact={true}
+                    component={RecipeCache}>
+                </GuardedRoute>
+                <GuardedRoute
+                    path='/settings'
+                    component={Settings}>
+                </GuardedRoute>
+                <GuardedRoute
+                    path='/recipes/:id'
+                    component={RecipeCache}>
+                </GuardedRoute>
+                <Redirect to="/" />
+                </Switch>
+            </BrowserRouter>
+            </ThemeProvider>
+        </QueryClientProvider>
+      )
+    }
+}
+
+export default App
