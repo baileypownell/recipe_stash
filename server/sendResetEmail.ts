67641995dae9e4f0642dfaea0c739e09e2929f04
Bailey Pownell <bailey.pownell@gmail.com>
2023-02-01 19:53:08 -0500

    diagnosing nodemailer

--- server/sendResetEmail.ts
+++ server/sendResetEmail.ts
@@ -47,6 +47,9 @@ router.post('/', (request: any, response, next) => {
 
             const mailer = nodemailer.createTransport({
               service: 'gmail',
+              host: 'smtp.gmail.com',
+              port: 465,
+              secure: true,
               auth: {
                 user: process.env.GOOGLE_EMAIL,
                 pass: process.env.GOOGLE_APP_PASSWORD,
