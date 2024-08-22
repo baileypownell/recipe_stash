2542a7fd5754d8c6a317ed4c4fbce753081336d9
Bailey Pownell <bailey.pownell@gmail.com>
2024-08-21 20:17:39 -0400

    build fix

--- public/src/components/Recipe/MobileRecipeToolbar.js
+++ public/src/components/Recipe/MobileRecipeToolbar.js
@@ -3,7 +3,6 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = MobileRecipeToolbar;
 const jsx_runtime_1 = require("react/jsx-runtime");
 const MoreVertRounded_1 = __importDefault(require("@mui/icons-material/MoreVertRounded"));
 const material_1 = require("@mui/material");
@@ -29,4 +28,5 @@ function MobileRecipeToolbar({ width, triggerDialog, cloneRecipe, }) {
             backgroundColor: theme.palette.gray.main,
         }, width: "100%", margin: "0", padding: "15px", zIndex: "1", children: [(0, jsx_runtime_1.jsx)(material_1.IconButton, { "aria-controls": "simple-menu", "aria-haspopup": "true", onClick: handleClick, color: "info", children: (0, jsx_runtime_1.jsx)(MoreVertRounded_1.default, {}) }), (0, jsx_runtime_1.jsxs)(material_1.Menu, { anchorEl: anchorEl, keepMounted: true, open: Boolean(anchorEl), onClose: handleClose, children: [(0, jsx_runtime_1.jsx)(material_1.MenuItem, { onClick: handleDialog, children: "Edit" }), (0, jsx_runtime_1.jsx)(material_1.MenuItem, { onClick: duplicateRecipe, children: "Duplicate" })] })] })) : null;
 }
+exports.default = MobileRecipeToolbar;
 //# sourceMappingURL=MobileRecipeToolbar.js.map
